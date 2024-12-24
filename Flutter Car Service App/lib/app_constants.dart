380f9a868df27cad26300cf895d01802bcc294a9
AbdulDayyan1git config --global user.name AbdulDayyan1git config --global user.email adkings24zak@gmail.com <adkings24zak@gmail.com>
2024-12-24 12:47:28 +0530

    Initial commit

---
+++ Flutter Car Service App/lib/app_constants.dart
@@ -0,0 +1,9 @@
+
+abstract class FFAppConstants {
+  static const String imageUrl =
+      'https://carservice.templatevilla.net/images/';
+  static const String apiKey = 'AIzaSyBvecUP300B0DvdGnGZA6BKUQwez2E44O8';
+  static const String appId = 'com.mycompany.carwash';
+  static const String messagingSenderId = '463819102155';
+  static const String projectId = 'carwashapp-4b688';
+}
